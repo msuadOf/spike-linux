639a1917bac70decfd824cdd54d7afa3ed8d5d10
白逸凡 <57826721+msuadOf@users.noreply.github.com>
2025-08-29 13:39:58 +0800

    Update build_busybox.mk

--- build_busybox.mk
+++ build_busybox.mk
@@ -10,7 +10,7 @@ initramfs rootfs:
 	#$(MAKE) -C busybox/ CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv  CONFIG_PREFIX=../initramfs menuconfig 
 	cp busybox_config busybox/.config
 	#$(MAKE) -C busybox/ CROSS_COMPILE=riscv64-linux-gnu- ARCH=riscv  CONFIG_PREFIX=../initramfs install
-	$(MAKE) -C busybox/ CROSS_COMPILE=/opt/riscv-rv32ima-ilp32/bin/riscv32-unknown-linux-gnu- ARCH=riscv  CONFIG_PREFIX=../initramfs install
+	$(MAKE) -C busybox/ CROSS_COMPILE=/opt/riscv/rv32ima-ilp32-gnu-linux/bin/riscv32-unknown-linux-gnu- ARCH=riscv  CONFIG_PREFIX=../initramfs install
 	sudo sh -c 'echo "#! /bin/sh" > ./initramfs/init'
 	sudo sh -c 'echo "exec /bin/sh" >> ./initramfs/init'
 	sudo sh -c 'chmod +x ./initramfs/init'
